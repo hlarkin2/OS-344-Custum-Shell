9a472e5d96b56ef9beb8ed4028ddd12f9ebb78ff
Hannah <hannah.larkin8@gmail.com>
2025-11-12 17:44:04 -0700

    switching languages

---
+++ main.c
@@ -0,0 +1,3 @@
+// this will be our main file to call functions from other files
+// it should not hold too much code as it's primary use is to run the shell, 
+// not hold the written functions, we will import them in
--- src/main.py
+++
@@ -1,3 +0,0 @@
-# this will be our main file to call functions from other files
-# it should not hold too much code as it's primary use is to run the shell, 
-# not hold the written functions, we will import them in
