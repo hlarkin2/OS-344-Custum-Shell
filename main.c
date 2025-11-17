edda098c88058526e66580e80eeb830f08a8975b
Hannah <hannah.larkin8@gmail.com>
2025-11-16 21:41:00 -0700

    created main loop and started input parsing

--- main.c
+++ main.c
@@ -24,3 +24,25 @@ char* read_input() {
     input[strcspn(input, "\n")] = 0;
     return input;
 }
+
+// parse inputs
+char** parse_input(char *input) {
+    char **args = malloc(MAX_ARGS * sizeof(char*));
+    if (!args) {
+        fprintf(stderr, "Allocation error\n");
+        exit(1);
+    }
+}
+
+int main() {
+    char *input;
+    char **args;
+    int status = 1;
+
+    // main loop
+    while (status) {
+        printf("customshell> ");
+    }
+
+    return 0;
+}
