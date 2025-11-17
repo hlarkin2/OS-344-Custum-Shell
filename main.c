f764994d2a6044baf9c6eb1317bab731faf843f3
Hannah <hannah.larkin8@gmail.com>
2025-11-16 21:35:05 -0700

    reading user input

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
-// this will be our main file to call functions from other files
-// it should not hold too much code as it's primary use is to run the shell, 
-// not hold the written functions, we will import them in
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 1024
+#define MAZ_ARGS 64
+
+// Read input from user
+char* read_input() {
+    char *input = malloc(MAX_INPUT);
+    if (!input) {
+        fprintf(stderr, "Allocation error\n");
+        exit(1);
+    }
+
+    if (fgets(input, MAX_INPUT, stdin) == NULL) {
+        free(input);
+        return NULL;
+    }
+
+    // remove newline character
+    input[strcspn(input, "\n")] = 0;
+    return input;
+}
